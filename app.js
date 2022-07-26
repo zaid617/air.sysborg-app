ea785dc71acf4b8df7fa3ed18df990c6f9e2d8e8
Zaid_ZiD <zid6177@gmail.com>
2022-07-26 17:22:41 +0500

    add logics in program

---
+++ app.js
@@ -0,0 +1,55 @@
+let notes = [];
+
+let getNotes = () => {
+    let takeNote = localStorage.getItem("newNote");
+    notes = JSON.parse(takeNote) || []
+    add()
+}
+
+getNotes();
+
+function addNotes() {
+    let note = document.getElementById('notes').value;
+
+    if (note == undefined) {
+        return 0
+    }
+    else {
+        notes.push(note)
+        localStorage.setItem("newNote", JSON.stringify(notes))
+        add();
+    }
+
+}
+
+
+function add() {
+
+    let section = document.getElementById('section');
+
+    for (let i = 0; i < notes.length; i++) {
+
+        section.innerHTML +=
+            `<div class="box">
+        <div class="info">
+        <p id="ip${i}" class="padding-3 size-17px underline"><strong>192.168.100.1</strong></p>
+        <p id="time${i}" class="padding-3 size-17px">2 minutes ago</p>
+        <button class="delete" class="padding-3 size-17px">Delete</button>
+        </div>
+        <div class="message" id="message${i}"></div>
+        </div>`
+
+        document.getElementById(`message${i}`).innerHTML = notes[i]
+    }
+
+}
+
+function clearFunc() {
+    let section = document.getElementById('section');
+    alert('Do you really want to delete all notes ?')
+    localStorage.removeItem('newNote')
+    section.innerHTML = "";
+    notes = [];
+}
+
+console.log(notes[7]);
