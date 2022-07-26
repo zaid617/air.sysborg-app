3801398ee9e1cee3d57ecbf31a979fe0bc981612
Zaid_ZiD <zid6177@gmail.com>
2022-07-26 18:42:32 +0500

    update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ let notes = [];
 let getNotes = () => {
     let takeNote = localStorage.getItem("newNote");
     notes = JSON.parse(takeNote) || []
-    add()
+    add2();
 }
 
 getNotes();
 
 function addNotes() {
     let note = document.getElementById('notes').value;
-
-    if (note === undefined) {
-        return 0
+    let none = ""
+    if (note === none) {
+        return;
     }
     else {
         notes.push(note)
@@ -23,11 +23,31 @@ function addNotes() {
 }
 
 
-function add() {
+function add2() {
 
     let section = document.getElementById('section');
 
     for (let i = 0; i < notes.length; i++) {
+
+        section.innerHTML +=
+            `<div class="box">
+        <div class="info">
+        <p id="ip${i}" class="padding-3 size-17px underline"><strong>192.168.100.1</strong></p>
+        <p id="time${i}" class="padding-3 size-17px">2 minutes ago</p>
+        <button class="delete" class="padding-3 size-17px">Delete</button>
+        </div>
+        <div class="message" id="message${i}"></div>
+        </div>`
+
+        document.getElementById(`message${i}`).innerHTML = notes[i]
+    }
+
+}
+function add() {
+
+    let section = document.getElementById('section');
+
+    for (let i = notes.length - 1; i < notes.length; i++) {
 
         section.innerHTML +=
             `<div class="box">
